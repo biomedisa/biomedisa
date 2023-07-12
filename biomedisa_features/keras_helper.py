b795e673c4ff8fe325c3916c578f0c772e493162
Philipp Loesel <philipp.loesel@anu.edu.au>
2023-07-12 13:48:50 +1000

    cleaned arguments; classification (beta version)

--- biomedisa_features/keras_helper.py
+++ biomedisa_features/keras_helper.py
@@ -636,16 +636,16 @@ def train_semantic_segmentation(normalize, img_list, label_list, x_scale, y_scal
               'dim_img': (zsh, ysh, xsh),
               'n_classes': nb_labels,
               'n_channels': channels,
-              'augment': (flip_x, flip_y, flip_z, rotate)}
+              'augment': (flip_x, flip_y, flip_z, False, rotate)}
 
     # data generator
     validation_generator = None
-    training_generator = DataGenerator(img, label, position, list_IDs, [], True, number_of_images, **params)
+    training_generator = DataGenerator(img, label, position, list_IDs, [], True, number_of_images, True, False, **params)
     if validation_split:
         if val_tf:
             params['dim_img'] = (zsh_val, ysh_val, xsh_val)
             params['augment'] = (False, False, False, 0)
-            validation_generator = DataGenerator(img_val, label_val, position_val, list_IDs_val, [], True, number_of_val_images, **params)
+            validation_generator = DataGenerator(img_val, label_val, position_val, list_IDs_val, [], True, number_of_val_images, False, False, **params)
         else:
             metrics = Metrics(img_val, label_val, list_IDs_val, (z_patch, y_patch, x_patch), (zsh_val, ysh_val, xsh_val), batch_size,
                               path_to_model, early_stopping, validation_freq, nb_labels, number_of_val_images)
