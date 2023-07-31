00ba9cfff23d9be759a36dc8456dda7ab906e963
Philipp Loesel <philipp.loesel@anu.edu.au>
2023-07-31 19:05:47 +1000

    remove asymm from R script

--- biomedisa_evaluation/natural_variability_statistics.R
+++ biomedisa_evaluation/natural_variability_statistics.R
@@ -1,3 +1,23 @@
+## ---------------------------
+##
+## Script name: Bee brain statistics
+##
+## Purpose of script: Supporting Information of Paper
+##
+## Author: Dr. Coline Monchanin
+##
+## Email: coline.monchanin@gmail.com
+##
+## ---------------------------
+##
+## This script is part of the publication:
+##
+## Lösel et al. Natural variability in bee brain size and symmetry revealed by micro-CT imaging and deep learning.
+##
+## Preprint at http://biorxiv.org/lookup/doi/10.1101/2022.10.12.511944 (2022).
+##
+## ---------------------------
+
 #library(multcomp)
 #library(emmeans)
 #library(ggplot2)
@@ -15,75 +35,14 @@ library(Hmisc)
 options( warn = -1 )
 
 ######ANALYSIS FOR HONEYBEES############
+HB=read.csv("HBBrain.csv", header=T, sep=";")
 
 #Brain size variation####
-HB=read.csv("HBBrain.csv", header=T, sep=";")
 (max(HB$Brain,na.rm=T)-min(HB$Brain,na.rm=T))*100/max(HB$Brain,na.rm=T)
-
-
 
 #Covariance####
 rcorr(as.matrix(HB[6:13]))
 rcorr(as.matrix(HB[13:20]))
-
-#Between colonies#####
-mod=lmer(Brain~Hive+(1|Date),data=HB)
-anova(mod)
-
-mod=lmer(AL~Hive+(1|Date),data=HB)
-anova(mod)
-
-mod=lmer(MB~Hive+(1|Date),data=HB)
-anova(mod)
-
-mod=lmer(CX~Hive+(1|Date),data=HB)
-anova(mod)
-
-mod=lmer(LO~Hive+(1|Date),data=HB)
-anova(mod)
-
-mod=lmer(ME~Hive+(1|Date),data=HB)
-anova(mod)
-
-mod=lmer(OL~Hive+(1|Date),data=HB)
-anova(mod)
-
-
-#Asymmetry####
-HB=read.csv("HBAsym.csv", header=T, sep=";")
-
-mod=lmer(AL~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(MB~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(ME~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(LO~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(OL~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-
-#Asymmetry relative volume####
-HB=read.csv("HBAsym.csv", header=T, sep=";")
-
-mod=lmer(ALr~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(MBr~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(MEr~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-mod=lmer(LOr~Side+(1|Date)+(1|ID),data=HB)
-anova(mod)
-
-
 
 #TABLE 1#####
 mod=lmer(Brain~Hive+(1|Site),data=HB)
@@ -95,9 +54,6 @@ anova(mod)
 mod=lmer(MB~Hive+(1|Site),data=HB)
 anova(mod)
 
-mod=lmer(MBr~Hive+(1|Site),data=HB)
-anova(mod)
-
 mod=lmer(OL~Hive+(1|Site),data=HB)
 anova(mod)
 
@@ -107,17 +63,13 @@ anova(mod)
 mod=lmer(ME~Hive+(1|Site),data=HB)
 anova(mod)
 
-#mod=lmer(CX~Hive+(1|Site),data=HB)
-#anova(mod)
+mod=lmer(CX~Hive+(1|Site),data=HB)
+anova(mod)
 
-#mod=lmer(OTH~Hive+(1|Site),data=HB)
-#anova(mod)
-
-
+mod=lmer(OTH~Hive+(1|Site),data=HB)
+anova(mod)
 
 #TABLE S3: % variability within hives#####
-HB=read.csv("HBBrain.csv", header=T, sep=";")
-
 H1=subset(HB,Hive=="H1")
 (max(H1$Brain,na.rm=T)-min(H1$Brain,na.rm=T))*100/max(H1$Brain,na.rm=T)
 (max(H1$AL,na.rm=T)-min(H1$AL,na.rm=T))*100/max(H1$AL,na.rm=T)
@@ -128,13 +80,10 @@ H1=subset(HB,Hive=="H1")
 (max(H1$ME,na.rm=T)-min(H1$ME,na.rm=T))*100/max(H1$ME,na.rm=T)
 (max(H1$LO,na.rm=T)-min(H1$LO,na.rm=T))*100/max(H1$LO,na.rm=T)
 
-
-
 ########ANALYSIS FOR BUMBLEBEES##########
-
-#Brain size variation#####
 BB=read.csv("BBBrain.csv", header=T, sep=";")
 
+#Brain size variation#####
 (max(BB$Brain,na.rm=T)-min(BB$Brain,na.rm=T))*100/max(BB$Brain,na.rm=T)
 (max(BB$AL,na.rm=T)-min(BB$AL,na.rm=T))*100/max(BB$AL,na.rm=T)
 (max(BB$MB,na.rm=T)-min(BB$MB,na.rm=T))*100/max(BB$MB,na.rm=T)
@@ -144,15 +93,11 @@ BB=read.csv("BBBrain.csv", header=T, sep=";")
 (max(BB$CX,na.rm=T)-min(BB$CX,na.rm=T))*100/max(BB$CX,na.rm=T)
 (max(BB$OTH,na.rm=T)-min(BB$OTH,na.rm=T))*100/max(BB$OTH,na.rm=T)
 
-
-
 ########Covariance####
-
 rcorr(as.matrix(BB[4:11]))
 rcorr(as.matrix(BB[11:18]))
 
 ####Between colonies#####
-
 mod=lm(Brain~Hive,data=BB)
 anova(mod)
 
@@ -177,12 +122,8 @@ anova(mod)
 mod=lm(OTH~Hive,data=BB)
 anova(mod)
 
-
 ####Within colonies######
-BB=read.csv("BBBrain.csv", header=T, sep=";")
-
 H1=subset(BB,Hive=="H1")
-
 (max(H1$Brain,na.rm=T)-min(H1$Brain,na.rm=T))*100/max(H1$Brain,na.rm=T)
 (max(H1$AL,na.rm=T)-min(H1$AL,na.rm=T))*100/max(H1$AL,na.rm=T)
 (max(H1$MB,na.rm=T)-min(H1$MB,na.rm=T))*100/max(H1$MB,na.rm=T)
@@ -191,42 +132,4 @@ H1=subset(BB,Hive=="H1")
 (max(H1$LO,na.rm=T)-min(H1$LO,na.rm=T))*100/max(H1$LO,na.rm=T)
 (max(H1$CX,na.rm=T)-min(H1$CX,na.rm=T))*100/max(H1$CX,na.rm=T)
 (max(H1$OTH,na.rm=T)-min(H1$OTH,na.rm=T))*100/max(H1$OTH,na.rm=T)
-
-
-
-#Asymmetry#####
-BB=read.csv("BBAsym.csv", header=T, sep=";")
-
-mod=lmer(AL~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(MB~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(ME~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(LO~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(OL~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-#Asymmetry relative volume####
-BB=read.csv("BBAsym.csv", header=T, sep=";")
-
-mod=lmer(ALr~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(MBr~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(MEr~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(LOr~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
-
-mod=lmer(OLr~Side+(1|ID)+(1|Hive),data=BB)
-anova(mod)
 
