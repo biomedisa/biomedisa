066b3d24cc67393b4d290e3ed09a04b9537698f4
Philipp Loesel <philipp.loesel@anu.edu.au>
2023-08-01 17:42:14 +1000

    Fig and Table names added to R script

--- biomedisa_evaluation/natural_variability_statistics.R
+++ biomedisa_evaluation/natural_variability_statistics.R
@@ -37,14 +37,21 @@ options( warn = -1 )
 ######ANALYSIS FOR HONEYBEES############
 HB=read.csv("HBBrain.csv", header=T, sep=";")
 
-#Brain size variation####
+#Brain size variation (Table 1)####
 (max(HB$Brain,na.rm=T)-min(HB$Brain,na.rm=T))*100/max(HB$Brain,na.rm=T)
+(max(HB$AL,na.rm=T)-min(HB$AL,na.rm=T))*100/max(HB$AL,na.rm=T)
+(max(HB$MB,na.rm=T)-min(HB$MB,na.rm=T))*100/max(HB$MB,na.rm=T)
+(max(HB$OL,na.rm=T)-min(HB$OL,na.rm=T))*100/max(HB$OL,na.rm=T)
+(max(HB$ME,na.rm=T)-min(HB$ME,na.rm=T))*100/max(HB$ME,na.rm=T)
+(max(HB$LO,na.rm=T)-min(HB$LO,na.rm=T))*100/max(HB$LO,na.rm=T)
+(max(HB$CX,na.rm=T)-min(HB$CX,na.rm=T))*100/max(HB$CX,na.rm=T)
+(max(HB$OTH,na.rm=T)-min(HB$OTH,na.rm=T))*100/max(HB$OTH,na.rm=T)
 
-#Covariance####
+#Covariance (Fig 5, S2 Fig, S2 Table)####
 rcorr(as.matrix(HB[6:13]))
 rcorr(as.matrix(HB[13:20]))
 
-#TABLE 1#####
+#Between colonies (Table 1 and Fig 6)#####
 mod=lmer(Brain~Hive+(1|Site),data=HB)
 anova(mod)
 
@@ -69,7 +76,7 @@ anova(mod)
 mod=lmer(OTH~Hive+(1|Site),data=HB)
 anova(mod)
 
-#TABLE S3: % variability within hives#####
+#Variability within hives (S3 Table)#####
 H1=subset(HB,Hive=="H1")
 (max(H1$Brain,na.rm=T)-min(H1$Brain,na.rm=T))*100/max(H1$Brain,na.rm=T)
 (max(H1$AL,na.rm=T)-min(H1$AL,na.rm=T))*100/max(H1$AL,na.rm=T)
@@ -83,7 +90,7 @@ H1=subset(HB,Hive=="H1")
 ########ANALYSIS FOR BUMBLEBEES##########
 BB=read.csv("BBBrain.csv", header=T, sep=";")
 
-#Brain size variation#####
+#Brain size variation (S4 Table)#####
 (max(BB$Brain,na.rm=T)-min(BB$Brain,na.rm=T))*100/max(BB$Brain,na.rm=T)
 (max(BB$AL,na.rm=T)-min(BB$AL,na.rm=T))*100/max(BB$AL,na.rm=T)
 (max(BB$MB,na.rm=T)-min(BB$MB,na.rm=T))*100/max(BB$MB,na.rm=T)
@@ -93,11 +100,11 @@ BB=read.csv("BBBrain.csv", header=T, sep=";")
 (max(BB$CX,na.rm=T)-min(BB$CX,na.rm=T))*100/max(BB$CX,na.rm=T)
 (max(BB$OTH,na.rm=T)-min(BB$OTH,na.rm=T))*100/max(BB$OTH,na.rm=T)
 
-########Covariance####
+########Covariance (S10 Fig, S11 Fig, S5 Table)####
 rcorr(as.matrix(BB[4:11]))
 rcorr(as.matrix(BB[11:18]))
 
-####Between colonies#####
+####Between colonies (S4 Table)#####
 mod=lm(Brain~Hive,data=BB)
 anova(mod)
 
