2edcd5f1069789fe95732f090c865a2ffa208046
Philipp Loesel <philipp.loesel@anu.edu.au>
2024-05-15 14:48:09 +1000

    split_volume updated and tmp directory removed

--- biomedisa_features/split_volume.py
+++ biomedisa_features/split_volume.py
@@ -1,6 +1,6 @@
 ##########################################################################
 ##                                                                      ##
-##  Copyright (c) 2024 Philipp Lösel. All rights reserved.              ##
+##  Copyright (c) 2019-2024 Philipp Lösel. All rights reserved.         ##
 ##                                                                      ##
 ##  This file is part of the open source project biomedisa.             ##
 ##                                                                      ##
@@ -30,245 +30,140 @@ import sys, os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 from biomedisa_features.biomedisa_helper import load_data, save_data
+from biomedisa_features.biomedisa_interpolation import smart_interpolation
+from tifffile import imread, imwrite, TiffFile
 import numpy as np
-import subprocess
-import platform
-import glob
+import argparse
 
 if __name__ == '__main__':
 
-    # path to data
-    path_to_data = sys.argv[1]
-    path_to_labels = sys.argv[2]
+    # initialize arguments
+    parser = argparse.ArgumentParser(description='Biomedisa interpolation.',
+             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    # get arguments
-    nump = 1
-    smooth = 0
-    overlap = 100
-    sub_z, sub_y, sub_x = 1, 1, 1
-    for i, val in enumerate(sys.argv):
-        if val in ['--split_z','-sz']:
-            sub_z = max(int(sys.argv[i+1]), 1)
-        if val in ['--split_y','-sy']:
-            sub_y = max(int(sys.argv[i+1]), 1)
-        if val in ['--split_x','-sx']:
-            sub_x = max(int(sys.argv[i+1]), 1)
-        if val in ['--overlap','-ol']:
-            overlap = max(int(sys.argv[i+1]), 0)
-        if val in ['-n','-np']:
-            nump = max(int(sys.argv[i+1]), 1)
-        if val in ['--smooth','-s']:
-            smooth = int(sys.argv[i+1])
-    uq = True if any(x in sys.argv for x in ['--uncertainty','-uq']) else False
-    allx = 1 if '-allx' in sys.argv else 0
+    # required arguments
+    parser.add_argument('path_to_data', type=str, metavar='PATH_TO_IMAGE',
+                        help='Location of image data')
+    parser.add_argument('path_to_labels', type=str, metavar='PATH_TO_LABELS',
+                        help='Location of label data')
 
-    # base directory
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # optional arguments
+    parser.add_argument('-allx', '--allaxis', action='store_true', default=False,
+                        help='If pre-segmentation is not exlusively in xy-plane')
+    parser.add_argument('-u', '--uncertainty', action='store_true', default=False,
+                        help='Return uncertainty of segmentation result')
+    parser.add_argument('-s', '--smooth', nargs='?', type=int, const=100, default=0,
+                        help='Number of smoothing iterations for segmentation result')
+    parser.add_argument('-ol', '--overlap', type=int, default=50,
+                        help='Overlap of sub-blocks')
+    parser.add_argument('-sx','--split_x', type=int, default=1,
+                        help='Number of sub-blocks in x-direction')
+    parser.add_argument('-sy','--split_y', type=int, default=1,
+                        help='Number of sub-blocks in y-direction')
+    parser.add_argument('-sz','--split_z', type=int, default=1,
+                        help='Number of sub-blocks in z-direction')
+    args = parser.parse_args()
 
-    # clean tmp folder
-    filelist = glob.glob(BASE_DIR+'/tmp/*.tif')
-    for f in filelist:
-        os.remove(f)
+    # image size
+    if args.path_to_data[-4:] == '.tif':
+        tif = TiffFile(args.path_to_data)
+        zsh = len(tif.pages)
+        ysh, xsh = tif.pages[0].shape
+    else:
+        print('Warning: This script is optimized for TIFF files. Please consider saving your data in TIFF format.')
+        data, _ = load_data(args.path_to_data)
+        shape = np.array(data.shape).copy()
+        zsh, ysh, xsh = shape
+        del data
 
-    # data shape
-    data, _ = load_data(path_to_data, 'split_volume')
-    shape = np.copy(np.array(data.shape), order='C')
-    zsh, ysh, xsh = shape
-    del data
+    if args.path_to_labels[-4:] != '.tif':
+        print('Warning: This script is optimized for TIFF files. Please consider saving your labels in TIFF format.')
 
     # split volume
-    sub_size_z = np.ceil(zsh / sub_z)
-    sub_size_y = np.ceil(ysh / sub_y)
-    sub_size_x = np.ceil(xsh / sub_x)
+    sub_size_z = np.ceil(zsh / args.split_z)
+    sub_size_y = np.ceil(ysh / args.split_y)
+    sub_size_x = np.ceil(xsh / args.split_x)
+
+    # allocate memory
+    final = np.zeros((zsh, ysh, xsh), dtype=np.uint8)
+    if args.smooth:
+        final_smooth = np.zeros_like(final)
+    if args.uncertainty:
+        final_uncertainty = np.zeros_like(final)
 
     # iterate over subvolumes
-    for sub_z_i in range(sub_z):
-        for sub_y_i in range(sub_y):
-            for sub_x_i in range(sub_x):
-                subvolume = sub_z_i*sub_y*sub_x + sub_y_i*sub_x + sub_x_i + 1
-                print('Subvolume:', subvolume, '/', sub_z*sub_y*sub_x)
+    for z_i in range(args.split_z):
+        for y_i in range(args.split_y):
+            for x_i in range(args.split_x):
+                subvolume = z_i * args.split_y * args.split_x + y_i * args.split_x + x_i + 1
+                print('Subvolume:', subvolume, '/', args.split_z * args.split_y * args.split_x)
 
                 # determine z subvolume
-                blockmin_z = int(sub_z_i * sub_size_z)
-                blockmax_z = int((sub_z_i+1) * sub_size_z)
-                datamin_z = max(blockmin_z - overlap, 0)
-                datamax_z = min(blockmax_z + overlap, zsh)
+                blockmin_z = int(z_i * sub_size_z)
+                blockmax_z = int((z_i+1) * sub_size_z)
+                datamin_z = max(blockmin_z - args.overlap, 0)
+                datamax_z = min(blockmax_z + args.overlap, zsh)
 
                 # determine y subvolume
-                blockmin_y = int(sub_y_i * sub_size_y)
-                blockmax_y = int((sub_y_i+1) * sub_size_y)
-                datamin_y = max(blockmin_y - overlap, 0)
-                datamax_y = min(blockmax_y + overlap, ysh)
+                blockmin_y = int(y_i * sub_size_y)
+                blockmax_y = int((y_i+1) * sub_size_y)
+                datamin_y = max(blockmin_y - args.overlap, 0)
+                datamax_y = min(blockmax_y + args.overlap, ysh)
 
                 # determine x subvolume
-                blockmin_x = int(sub_x_i * sub_size_x)
-                blockmax_x = int((sub_x_i+1) * sub_size_x)
-                datamin_x = max(blockmin_x - overlap, 0)
-                datamax_x = min(blockmax_x + overlap, xsh)
+                blockmin_x = int(x_i * sub_size_x)
+                blockmax_x = int((x_i+1) * sub_size_x)
+                datamin_x = max(blockmin_x - args.overlap, 0)
+                datamax_x = min(blockmax_x + args.overlap, xsh)
 
                 # extract image subvolume
-                data, _ = load_data(path_to_data, 'split_volume')
-                save_data(BASE_DIR+f'/tmp/sub_volume_{subvolume}.tif', data[datamin_z:datamax_z,datamin_y:datamax_y,datamin_x:datamax_x], False)
-                del data
+                if args.path_to_data[-4:] == '.tif':
+                    data = imread(args.path_to_data, key=range(datamin_z,datamax_z))
+                    data = data[:,datamin_y:datamax_y,datamin_x:datamax_x].copy()
+                else:
+                    data, _ = load_data(args.path_to_data)
+                    data = data[datamin_z:datamax_z,datamin_y:datamax_y,datamin_x:datamax_x].copy()
 
                 # extract label subvolume
-                labelData, header, final_image_type = load_data(path_to_labels, 'split_volume', True)
-                save_data(BASE_DIR+'/tmp/labels.sub_volume.tif', labelData[datamin_z:datamax_z,datamin_y:datamax_y,datamin_x:datamax_x])
-                del labelData
-
-                # configure command
-                cmd = ['mpiexec', '-np', f'{nump}', 'python3', 'biomedisa_interpolation.py', BASE_DIR+f'/tmp/sub_volume_{subvolume}.tif', BASE_DIR+'/tmp/labels.sub_volume.tif', '-s', f'{smooth}']
-                if uq:
-                    cmd.append('-uq')
-                if allx:
-                    cmd.append('-allx')
-                cwd = BASE_DIR + '/demo/'
-
-                # run segmentation
-                if platform.system() == 'Windows':
-                    cmd[3] = 'python'
-                    cmd.insert(4, '-u')
-                    p = subprocess.Popen(cmd, cwd=cwd, stdout=subprocess.PIPE)
-                    for line in iter(p.stdout.readline, b''):
-                        line = str(line,'utf-8')
-                        print(line.rstrip())
-                    p.stdout.close()
+                if args.path_to_labels[-4:] == '.tif':
+                    header, final_image_type = None, '.tif'
+                    labelData = imread(args.path_to_labels, key=range(datamin_z,datamax_z))
+                    labelData = labelData[:,datamin_y:datamax_y,datamin_x:datamax_x].copy()
                 else:
-                    p = subprocess.Popen(cmd, cwd=cwd)
-                p.wait()
+                    labelData, header, final_image_type = load_data(args.path_to_labels, return_extension=True)
+                    labelData = labelData[datamin_z:datamax_z,datamin_y:datamax_y,datamin_x:datamax_x].copy()
 
-                # remove tmp files
-                os.remove(BASE_DIR+f'/tmp/sub_volume_{subvolume}.tif')
-                os.remove(BASE_DIR+'/tmp/labels.sub_volume.tif')
+                # interpolation
+                results = smart_interpolation(data, labelData, uncertainty=args.uncertainty, allaxis=args.allaxis, smooth=args.smooth)
 
-    # create path_to_final
-    filename, extension = os.path.splitext(os.path.basename(path_to_data))
+                # append results
+                final[blockmin_z:blockmax_z,blockmin_y:blockmax_y,blockmin_x:blockmax_x] \
+                    = results['regular'][blockmin_z-datamin_z:blockmax_z-datamin_z,blockmin_y-datamin_y:blockmax_y-datamin_y,blockmin_x-datamin_x:blockmax_x-datamin_x]
+                if 'smooth' in results and results['smooth'] is not None:
+                    final_smooth[blockmin_z:blockmax_z,blockmin_y:blockmax_y,blockmin_x:blockmax_x] \
+                        = results['smooth'][blockmin_z-datamin_z:blockmax_z-datamin_z,blockmin_y-datamin_y:blockmax_y-datamin_y,blockmin_x-datamin_x:blockmax_x-datamin_x]
+                if 'uncertainty' in results and results['uncertainty'] is not None:
+                    final_uncertainty[blockmin_z:blockmax_z,blockmin_y:blockmax_y,blockmin_x:blockmax_x] \
+                        = results['uncertainty'][blockmin_z-datamin_z:blockmax_z-datamin_z,blockmin_y-datamin_y:blockmax_y-datamin_y,blockmin_x-datamin_x:blockmax_x-datamin_x]
+
+    # path to regular result
+    filename, extension = os.path.splitext(os.path.basename(args.path_to_data))
     if extension == '.gz':
         filename = filename[:-4]
     filename = 'final.' + filename
-    path_to_final = path_to_data.replace(os.path.basename(path_to_data), filename + final_image_type)
+    path_to_final = args.path_to_data.replace(os.path.basename(args.path_to_data), filename + final_image_type)
 
-    # path_to_uq and path_to_smooth
+    # path to optional results
     filename, extension = os.path.splitext(path_to_final)
     if extension == '.gz':
         filename = filename[:-4]
     path_to_smooth = filename + '.smooth' + final_image_type
     path_to_uq = filename + '.uncertainty.tif'
 
-    # iterate over subvolumes
-    final = np.zeros((zsh, ysh, xsh), dtype=np.uint8)
-    for sub_z_i in range(sub_z):
-        for sub_y_i in range(sub_y):
-            for sub_x_i in range(sub_x):
-                subvolume = sub_z_i*sub_y*sub_x + sub_y_i*sub_x + sub_x_i + 1
-                print('Subvolume:', subvolume, '/', sub_z*sub_y*sub_x)
-
-                # determine z subvolume
-                blockmin_z = int(sub_z_i * sub_size_z)
-                blockmax_z = int((sub_z_i+1) * sub_size_z)
-                datamin_z = max(blockmin_z - overlap, 0)
-                datamax_z = min(blockmax_z + overlap, zsh)
-
-                # determine y subvolume
-                blockmin_y = int(sub_y_i * sub_size_y)
-                blockmax_y = int((sub_y_i+1) * sub_size_y)
-                datamin_y = max(blockmin_y - overlap, 0)
-                datamax_y = min(blockmax_y + overlap, ysh)
-
-                # determine x subvolume
-                blockmin_x = int(sub_x_i * sub_size_x)
-                blockmax_x = int((sub_x_i+1) * sub_size_x)
-                datamin_x = max(blockmin_x - overlap, 0)
-                datamax_x = min(blockmax_x + overlap, xsh)
-
-                # load subvolume
-                path_to_subvolume = BASE_DIR+f'/tmp/final.sub_volume_{subvolume}.tif'
-                if os.path.isfile(path_to_subvolume):
-                    tmp, _ = load_data(path_to_subvolume)
-                    final[blockmin_z:blockmax_z,blockmin_y:blockmax_y,blockmin_x:blockmax_x] \
-                    = tmp[blockmin_z-datamin_z:blockmax_z-datamin_z,blockmin_y-datamin_y:blockmax_y-datamin_y,blockmin_x-datamin_x:blockmax_x-datamin_x]
-                    os.remove(path_to_subvolume)
-
-    # save result
+    # save results
     save_data(path_to_final, final, header)
-
-    # iterate over subvolumes (smooth)
-    smooth = 0
-    final.fill(0)
-    for sub_z_i in range(sub_z):
-        for sub_y_i in range(sub_y):
-            for sub_x_i in range(sub_x):
-                subvolume = sub_z_i*sub_y*sub_x + sub_y_i*sub_x + sub_x_i + 1
-                print('Subvolume:', subvolume, '/', sub_z*sub_y*sub_x)
-
-                # determine z subvolume
-                blockmin_z = int(sub_z_i * sub_size_z)
-                blockmax_z = int((sub_z_i+1) * sub_size_z)
-                datamin_z = max(blockmin_z - overlap, 0)
-                datamax_z = min(blockmax_z + overlap, zsh)
-
-                # determine y subvolume
-                blockmin_y = int(sub_y_i * sub_size_y)
-                blockmax_y = int((sub_y_i+1) * sub_size_y)
-                datamin_y = max(blockmin_y - overlap, 0)
-                datamax_y = min(blockmax_y + overlap, ysh)
-
-                # determine x subvolume
-                blockmin_x = int(sub_x_i * sub_size_x)
-                blockmax_x = int((sub_x_i+1) * sub_size_x)
-                datamin_x = max(blockmin_x - overlap, 0)
-                datamax_x = min(blockmax_x + overlap, xsh)
-
-                # load subvolume
-                path_to_subvolume = BASE_DIR+f'/tmp/final.sub_volume_{subvolume}.smooth.tif'
-                if os.path.isfile(path_to_subvolume):
-                    tmp, _ = load_data(path_to_subvolume)
-                    final[blockmin_z:blockmax_z,blockmin_y:blockmax_y,blockmin_x:blockmax_x] \
-                    = tmp[blockmin_z-datamin_z:blockmax_z-datamin_z,blockmin_y-datamin_y:blockmax_y-datamin_y,blockmin_x-datamin_x:blockmax_x-datamin_x]
-                    os.remove(path_to_subvolume)
-                    smooth = 1
-
-    # save result
-    if smooth:
-        save_data(path_to_smooth, final, header)
-
-    # iterate over subvolumes (uncertainty)
-    uncertainty = 0
-    final.fill(0)
-    for sub_z_i in range(sub_z):
-        for sub_y_i in range(sub_y):
-            for sub_x_i in range(sub_x):
-                subvolume = sub_z_i*sub_y*sub_x + sub_y_i*sub_x + sub_x_i + 1
-                print('Subvolume:', subvolume, '/', sub_z*sub_y*sub_x)
-
-                # determine z subvolume
-                blockmin_z = int(sub_z_i * sub_size_z)
-                blockmax_z = int((sub_z_i+1) * sub_size_z)
-                datamin_z = max(blockmin_z - overlap, 0)
-                datamax_z = min(blockmax_z + overlap, zsh)
-
-                # determine y subvolume
-                blockmin_y = int(sub_y_i * sub_size_y)
-                blockmax_y = int((sub_y_i+1) * sub_size_y)
-                datamin_y = max(blockmin_y - overlap, 0)
-                datamax_y = min(blockmax_y + overlap, ysh)
-
-                # determine x subvolume
-                blockmin_x = int(sub_x_i * sub_size_x)
-                blockmax_x = int((sub_x_i+1) * sub_size_x)
-                datamin_x = max(blockmin_x - overlap, 0)
-                datamax_x = min(blockmax_x + overlap, xsh)
-
-                # load subvolume
-                path_to_subvolume = BASE_DIR+f'/tmp/final.sub_volume_{subvolume}.uncertainty.tif'
-                if os.path.isfile(path_to_subvolume):
-                    tmp, _ = load_data(path_to_subvolume)
-                    final[blockmin_z:blockmax_z,blockmin_y:blockmax_y,blockmin_x:blockmax_x] \
-                    = tmp[blockmin_z-datamin_z:blockmax_z-datamin_z,blockmin_y-datamin_y:blockmax_y-datamin_y,blockmin_x-datamin_x:blockmax_x-datamin_x]
-                    os.remove(path_to_subvolume)
-                    uncertainty = 1
-
-    # save result
-    if uncertainty:
-        save_data(path_to_uq, final, header)
+    if args.smooth:
+        save_data(path_to_smooth, final_smooth, header)
+    if args.uncertainty:
+        imwrite(path_to_uq, final_uncertainty)
 
