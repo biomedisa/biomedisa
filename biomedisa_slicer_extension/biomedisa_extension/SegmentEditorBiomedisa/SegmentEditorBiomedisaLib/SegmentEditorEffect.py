c60faeb0d1b8df09fa36e929c11a48238c5951a1
Matthias Fabian <matze.fabian@gmail.com>
2024-07-12 13:53:26 +0200

    Rework biomedisa into a segment editor effect

---
+++ biomedisa_slicer_extension/biomedisa_extension/SegmentEditorBiomedisa/SegmentEditorBiomedisaLib/SegmentEditorEffect.py
@@ -0,0 +1,106 @@
+import os
+import vtk, qt, slicer
+import logging
+from SegmentEditorEffects import *
+from Logic.BiomedisaLogic import BiomedisaLogic
+
+# Source: https://github.com/lassoan/SlicerSegmentEditorExtraEffects
+class SegmentEditorEffect(AbstractScriptedSegmentEditorEffect):
+  """This effect uses the Biomedisa algorithm to segment large 3D volumetric images"""
+
+  def __init__(self, scriptedEffect):
+    scriptedEffect.name = 'Biomedisa'
+    scriptedEffect.perSegment = True # this effect operates on a single selected segment
+    AbstractScriptedSegmentEditorEffect.__init__(self, scriptedEffect)
+
+  def clone(self):
+    # It should not be necessary to modify this method
+    import qSlicerSegmentationsEditorEffectsPythonQt as effects
+    clonedEffect = effects.qSlicerSegmentEditorScriptedEffect(None)
+    clonedEffect.setPythonSource(__file__.replace('\\','/'))
+    return clonedEffect
+
+  def icon(self):
+    # It should not be necessary to modify this method
+    iconPath = os.path.join(os.path.dirname(__file__), 'SegmentEditorEffect.png')
+    if os.path.exists(iconPath):
+      return qt.QIcon(iconPath)
+    return qt.QIcon()
+
+  def helpText(self):
+    return """<html>Biomedisa is a free and easy-to-use open-source application for segmenting large volumetric images such as CT and MRI scans,
+developed at The Australian National University CTLab. Biomedisa's smart interpolation of sparsely pre-segmented slices
+enables accurate semi-automated segmentation by considering the complete underlying image data. 
+For more information visit the <a href="https://biomedisa.info/">project page</a>.
+<p></html>"""
+
+  def setupOptionsFrame(self):
+    
+    self.allaxis = qt.QCheckBox()
+    #self.allaxis.value = False
+    self.scriptedEffect.addLabeledOptionsWidget("allaxis:", self.allaxis)
+
+    self.nbrw = qt.QSpinBox()
+    self.nbrw.minimum = 1
+    self.nbrw.maximum = 1000
+    self.nbrw.value = 10
+    self.scriptedEffect.addLabeledOptionsWidget("nbrw:", self.nbrw)
+    
+    self.sorw = qt.QSpinBox()
+    self.sorw.minimum = 1
+    self.sorw.maximum = 1000000
+    self.sorw.value = 4000
+    self.scriptedEffect.addLabeledOptionsWidget("sorw:", self.sorw)
+
+
+    self.runButton = qt.QPushButton("Run")
+    self.runButton.objectName = self.__class__.__name__ + 'Run'
+    self.runButton.setToolTip("Run the biomedisa algorithm and generate segment data")
+
+
+    self.cancelButton = qt.QPushButton("Cancel")
+    self.cancelButton.objectName = self.__class__.__name__ + 'Cancel'
+    self.cancelButton.setToolTip("Clear preview and cancel")
+
+    self.applyButton = qt.QPushButton("Apply")
+    self.applyButton.objectName = self.__class__.__name__ + 'Apply'
+    self.applyButton.setToolTip("Run the biomedisa algorithm and generate segment data")
+
+    self.scriptedEffect.addOptionsWidget(self.runButton)
+
+    self.runButton.connect('clicked()', self.onRun)
+
+  def createCursor(self, widget):
+    # TODO: Change cursor to make it obvious you are about to do something
+    return slicer.util.mainWindow().cursor
+
+  def onRun(self):
+    # This can be a long operation - indicate it to the user
+    qt.QApplication.setOverrideCursor(qt.Qt.WaitCursor)
+    try:
+      slicer.util.showStatusMessage('Running Biomedisa...', 2000)
+      self.scriptedEffect.saveStateForUndo()
+      self.biomedisa()
+      slicer.util.showStatusMessage('Biomedisa finished', 2000)
+
+    finally:
+      qt.QApplication.restoreOverrideCursor()
+
+  def biomedisa(self):
+    segmentID = self.scriptedEffect.parameterSetNode().GetSelectedSegmentID()
+    segmentationNode = self.scriptedEffect.parameterSetNode().GetSegmentationNode()
+    segment = segmentationNode.GetSegmentation().GetSegment(segmentID)
+
+    # Get modifier labelmap
+    binaryLabelmap = segment.GetRepresentation(slicer.vtkSegmentationConverter.GetSegmentationBinaryLabelmapRepresentationName())
+    # Get source volume image data
+    sourceImageData = self.scriptedEffect.sourceVolumeImageData()
+
+    resultLabelMap = BiomedisaLogic.runBiomedisa(input=sourceImageData, labels=binaryLabelmap)
+
+    slicer.vtkSlicerSegmentationsModuleLogic.SetBinaryLabelmapToSegment(
+      resultLabelMap, 
+      segmentationNode, 
+      segmentID, 
+      slicer.vtkSlicerSegmentationsModuleLogic.MODE_REPLACE, 
+      resultLabelMap.GetExtent())
