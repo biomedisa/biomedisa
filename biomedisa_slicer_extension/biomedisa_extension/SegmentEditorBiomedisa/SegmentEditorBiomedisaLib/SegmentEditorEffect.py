a557643097c014c0acb6e02cf22dca38179fef26
Matthias Fabian <matze.fabian@gmail.com>
2024-07-16 10:02:31 +0200

    Add biomedisa parameter

--- biomedisa_slicer_extension/biomedisa_extension/SegmentEditorBiomedisa/SegmentEditorBiomedisaLib/SegmentEditorEffect.py
+++ biomedisa_slicer_extension/biomedisa_extension/SegmentEditorBiomedisa/SegmentEditorBiomedisaLib/SegmentEditorEffect.py
@@ -1,8 +1,10 @@
 import os
-import vtk, qt, slicer
+import vtk, qt, ctk, slicer
 import logging
 from SegmentEditorEffects import *
 from Logic.BiomedisaLogic import BiomedisaLogic
+from Logic.BiomedisaParameter import BiomedisaParameter
+from biomedisa.features.biomedisa_helper import _get_platform
 
 # Source: https://github.com/lassoan/SlicerSegmentEditorExtraEffects
 class SegmentEditorEffect(AbstractScriptedSegmentEditorEffect):
@@ -35,28 +37,53 @@ For more information visit the <a href="https://biomedisa.info/">project page</a
 <p></html>"""
 
   def setupOptionsFrame(self):
-    
+    collapsibleButton = ctk.ctkCollapsibleButton()
+    collapsibleButton.text = "Advanced"
+    collapsibleButton.setChecked(True)
+    self.scriptedEffect.addOptionsWidget(collapsibleButton)
+
+    collapsibleLayout = qt.QFormLayout(collapsibleButton)
+
     self.allaxis = qt.QCheckBox()
-    #self.allaxis.value = False
-    self.scriptedEffect.addLabeledOptionsWidget("allaxis:", self.allaxis)
+    self.allaxis.toolTip = 'If pre-segmentation is not exlusively in xy-plane'
+    collapsibleLayout.addRow("allaxis:", self.allaxis)
+
+    self.denoise = qt.QCheckBox()
+    self.denoise.toolTip = 'Smooth/denoise image data before processing'
+    collapsibleLayout.addRow("denoise:", self.denoise)
 
     self.nbrw = qt.QSpinBox()
+    self.nbrw.toolTip = 'Number of random walks starting at each pre-segmented pixel'
     self.nbrw.minimum = 1
     self.nbrw.maximum = 1000
     self.nbrw.value = 10
-    self.scriptedEffect.addLabeledOptionsWidget("nbrw:", self.nbrw)
+    collapsibleLayout.addRow("nbrw:", self.nbrw)
     
     self.sorw = qt.QSpinBox()
+    self.sorw.toolTip = 'Steps of a random walk'
     self.sorw.minimum = 1
     self.sorw.maximum = 1000000
     self.sorw.value = 4000
-    self.scriptedEffect.addLabeledOptionsWidget("sorw:", self.sorw)
+    collapsibleLayout.addRow("sorw:", self.sorw)
 
+    self.ignore = qt.QLineEdit()
+    self.ignore.text = 'none'
+    self.ignore.toolTip = 'Ignore specific label(s), e.g. 2,5,6'
+    collapsibleLayout.addRow("ignore:", self.ignore)
+
+    self.only = qt.QLineEdit()
+    self.only.text = 'all'
+    self.only.toolTip = 'Segment only specific label(s), e.g. 1,3,5'
+    collapsibleLayout.addRow("only:", self.only)
+
+    self.platform = qt.QLineEdit()
+    self.platform.text = self.getPlatform()
+    self.platform.toolTip = 'One of "cuda", "opencl_NVIDIA_GPU", "opencl_Intel_CPU"'
+    collapsibleLayout.addRow("platform:", self.platform)
 
     self.runButton = qt.QPushButton("Run")
     self.runButton.objectName = self.__class__.__name__ + 'Run'
     self.runButton.setToolTip("Run the biomedisa algorithm and generate segment data")
-
 
     self.cancelButton = qt.QPushButton("Cancel")
     self.cancelButton.objectName = self.__class__.__name__ + 'Cancel'
@@ -70,6 +97,30 @@ For more information visit the <a href="https://biomedisa.info/">project page</a
 
     self.runButton.connect('clicked()', self.onRun)
 
+  def getPlatform(self) -> str:
+    class Biomedisa:
+      def __init__(self, platform=None, success=True, available_devices=0):
+        self.platform = platform
+        self.success = success
+        self.available_devices = available_devices
+
+    result = Biomedisa(platform=None, success=True, available_devices=0)
+    _get_platform(result)
+    if(result.success):
+      return result.platform
+    return 'None'
+
+  def getBiomedisaParameter(self) -> BiomedisaParameter:
+    parameter = BiomedisaParameter()
+    parameter.allaxis = self.allaxis.isChecked()
+    parameter.denoise = self.denoise.isChecked()
+    parameter.nbrw = self.nbrw.value
+    parameter.sorw = self.sorw.value
+    parameter.ignore = self.ignore.text
+    parameter.only = self.only.text
+    parameter.platform = self.platform.text
+    return parameter
+    
   def createCursor(self, widget):
     # TODO: Change cursor to make it obvious you are about to do something
     return slicer.util.mainWindow().cursor
@@ -96,8 +147,10 @@ For more information visit the <a href="https://biomedisa.info/">project page</a
     binaryLabelmap = segment.GetRepresentation(slicer.vtkSegmentationConverter.GetSegmentationBinaryLabelmapRepresentationName())
     # Get source volume image data
     sourceImageData = self.scriptedEffect.sourceVolumeImageData()
+
+    parameter = self.getBiomedisaParameter()
     # Run the algorithm
-    resultLabelMaps = BiomedisaLogic.runBiomedisa(input=sourceImageData, labels=binaryLabelmap)
+    resultLabelMaps = BiomedisaLogic.runBiomedisa(input=sourceImageData, labels=binaryLabelmap, parameter=parameter)
     
     for label, binaryLabelmap in resultLabelMaps:
       # Get segment ID from label index. This is 0 based even though first the voxel value is 1.
