d3305d88f5dcef8784545439bb9a9f6409004ee2
Matthias Fabian <matze.fabian@gmail.com>
2024-07-17 11:43:03 +0200

    Add biomedisa deep learning effect

---
+++ biomedisa_slicer_extension/biomedisa_extension/SegmentEditorBiomedisaDeepLearning/SegmentEditorBiomedisaDeepLearning.py
@@ -0,0 +1,28 @@
+import os
+import slicer
+from slicer.ScriptedLoadableModule import *
+
+# Source: https://github.com/lassoan/SlicerSegmentEditorExtraEffects
+class SegmentEditorBiomedisaDeepLearning(ScriptedLoadableModule):
+    """Uses ScriptedLoadableModule base class, available at:
+    https://github.com/Slicer/Slicer/blob/main/Base/Python/slicer/ScriptedLoadableModule.py
+    """
+
+    def __init__(self, parent):
+        import string
+        ScriptedLoadableModule.__init__(self, parent)
+        self.parent.title = "SegmentEditorBiomedisaDeepLearning"
+        self.parent.categories = ["Segmentation"]
+        self.parent.dependencies = ["Segmentations"]
+        self.parent.contributors = ["Matthias Fabian"]
+        self.parent.hidden = True
+        self.parent.helpText = "This hidden module registers the segment editor effect"
+        self.parent.helpText += self.getDefaultModuleDocumentationLink()
+        slicer.app.connect("startupCompleted()", self.registerEditorEffect)
+
+    def registerEditorEffect(self):
+        import qSlicerSegmentationsEditorEffectsPythonQt as qSlicerSegmentationsEditorEffects
+        instance = qSlicerSegmentationsEditorEffects.qSlicerSegmentEditorScriptedEffect(None)
+        effectFilename = os.path.join(os.path.dirname(__file__), self.__class__.__name__+'Lib/SegmentEditorEffect.py')
+        instance.setPythonSource(effectFilename.replace('\\','/'))
+        instance.self().register()
