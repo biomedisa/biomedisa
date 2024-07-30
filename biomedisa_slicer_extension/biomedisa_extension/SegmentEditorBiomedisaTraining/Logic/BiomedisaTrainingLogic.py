53aee7d87b28eac71aed6f637994b80bcdaa5ffa
Philipp Loesel <philipp.loesel@anu.edu.au>
2024-07-30 17:11:20 +1000

    apply user defined cropping to subvolume for training

--- biomedisa_slicer_extension/biomedisa_extension/SegmentEditorBiomedisaTraining/Logic/BiomedisaTrainingLogic.py
+++ biomedisa_slicer_extension/biomedisa_extension/SegmentEditorBiomedisaTraining/Logic/BiomedisaTrainingLogic.py
@@ -10,14 +10,14 @@ class BiomedisaTrainingLogic():
 
     def _expandLabelToMatchInputImage(labelImageData, inputDimensions) -> vtk.vtkImageData:
         # Initialize the new VTK image data object with the same dimensions as the input image
-        newLabelImageData = vtk.vtkImageData()
-        newLabelImageData.SetDimensions(inputDimensions)
-        newLabelImageData.AllocateScalars(vtk.VTK_UNSIGNED_CHAR, 1)
+        #newLabelImageData = vtk.vtkImageData()
+        #newLabelImageData.SetDimensions(inputDimensions)
+        #newLabelImageData.AllocateScalars(vtk.VTK_UNSIGNED_CHAR, 1)
 
         # Get the bounds and extent of the original label image data
-        labelBounds = labelImageData.GetBounds()
+        #labelBounds = labelImageData.GetBounds()
         labelExtent = labelImageData.GetExtent()
-        
+
         # Convert the label image data to a NumPy array
         labelPointData = labelImageData.GetPointData()
         labelVtkArray = labelPointData.GetScalars()
@@ -29,43 +29,54 @@ class BiomedisaTrainingLogic():
         newLabelNumpyArray = newLabelNumpyArray.reshape(inputDimensions[::-1])
 
         # Calculate the offset for copying the label data to the correct position in the new image
-        offsets = [-labelBounds[1] + 0.5,
-                   -labelBounds[3] + 0.5,
-                   labelBounds[4] + 0.5]
+        #offsets = [-labelBounds[1] + 0.5,
+        #           -labelBounds[3] + 0.5,
+        #           labelBounds[4] + 0.5]
 
         # Copy label data to the new image data at the correct position
         zmin, zmax = labelExtent[4], labelExtent[5] + 1
         ymin, ymax = labelExtent[2], labelExtent[3] + 1
         xmin, xmax = labelExtent[0], labelExtent[1] + 1
-        zminOffset, zmaxOffset = int(round(zmin-offsets[2])), int(round(zmax-offsets[2]))
-        yminOffset, ymaxOffset = int(round(ymin-offsets[1])), int(round(ymax-offsets[1]))
-        xminOffset, xmaxOffset = int(round(xmin-offsets[0])), int(round(xmax-offsets[0]))
-        newLabelNumpyArray[zmin:zmax, ymin:ymax, xmin:xmax] \
-            = labelNumpyArray[zminOffset:zmaxOffset, yminOffset:ymaxOffset, xminOffset:xmaxOffset]
+        #zminOffset, zmaxOffset = int(round(zmin-offsets[2])), int(round(zmax-offsets[2]))
+        #yminOffset, ymaxOffset = int(round(ymin-offsets[1])), int(round(ymax-offsets[1]))
+        #xminOffset, xmaxOffset = int(round(xmin-offsets[0])), int(round(xmax-offsets[0]))
+        newLabelNumpyArray[zmin:zmax, ymin:ymax, xmin:xmax] = labelNumpyArray
+        #= labelNumpyArray[zminOffset:zmaxOffset, yminOffset:ymaxOffset, xminOffset:xmaxOffset]
 
         # Convert the NumPy array back to a VTK array and set it as the scalars of the new VTK image data object
-        newLabelVtkArray = vtk_np.numpy_to_vtk(newLabelNumpyArray.ravel(), deep=True, array_type=vtk.VTK_UNSIGNED_CHAR)
-        newLabelImageData.GetPointData().SetScalars(newLabelVtkArray)
-        return newLabelImageData
-    
+        #newLabelVtkArray = vtk_np.numpy_to_vtk(newLabelNumpyArray.ravel(), deep=True, array_type=vtk.VTK_UNSIGNED_CHAR)
+        #newLabelImageData.GetPointData().SetScalars(newLabelVtkArray)
+        return newLabelNumpyArray
+
     def trainDeepLearning(
             input: vtkMRMLScalarVolumeNode,
-            labels: vtkMRMLLabelMapVolumeNode, 
+            labels: vtkMRMLLabelMapVolumeNode,
             parameter: BiomedisaTrainingParameter):
-        
+
         dimensions = input.GetDimensions()
-        x_range = [parameter.x_min, parameter.x_max] if parameter.x_min > 0 or dimensions[0] > parameter.x_max else None
-        y_range = [parameter.y_min, parameter.y_max] if parameter.y_min > 0 or dimensions[1] > parameter.y_max else None
-        z_range = [parameter.z_min, parameter.z_max] if parameter.z_min > 0 or dimensions[2] > parameter.z_max else None
-        extendedLabel = BiomedisaTrainingLogic._expandLabelToMatchInputImage(labels, dimensions)
-        numpyLabels = Helper._vtkToNumpy(extendedLabel)
+        x_range = [parameter.x_min, parameter.x_max] if parameter.x_min > 0 or dimensions[0]-1 > parameter.x_max else None
+        y_range = [parameter.y_min, parameter.y_max] if parameter.y_min > 0 or dimensions[1]-1 > parameter.y_max else None
+        z_range = [parameter.z_min, parameter.z_max] if parameter.z_min > 0 or dimensions[2]-1 > parameter.z_max else None
+        numpyLabels = BiomedisaTrainingLogic._expandLabelToMatchInputImage(labels, dimensions)
+        #numpyLabels = Helper._vtkToNumpy(extendedLabel)
         numpyImage = Helper._vtkToNumpy(input)
 
+        # crop training data
+        if z_range is not None:
+            numpyImage = numpyImage[z_range[0]:z_range[1]+1].copy()
+            numpyLabels = numpyLabels[z_range[0]:z_range[1]+1].copy()
+        if y_range is not None:
+            numpyImage = numpyImage[:,y_range[0]:y_range[1]+1].copy()
+            numpyLabels = numpyLabels[:,y_range[0]:y_range[1]+1].copy()
+        if x_range is not None:
+            numpyImage = numpyImage[:,:,x_range[0]:x_range[1]+1].copy()
+            numpyLabels = numpyLabels[:,:,x_range[0]:x_range[1]+1].copy()
+
         print(f"Running biomedisa training with: {parameter}")
-        
+
         from biomedisa.deeplearning import deep_learning
         deep_learning(
-            img_data=numpyImage, 
+            img_data=numpyImage,
             label_data=numpyLabels,
             path_to_model=parameter.path_to_model,
             stride_size=parameter.stride_size,
@@ -77,10 +88,8 @@ class BiomedisaTrainingLogic():
             flip_y=parameter.flip_y,
             flip_z=parameter.flip_z,
             scaling=parameter.scaling,
-            x_scale=parameter.x_scale, 
-            y_scale=parameter.y_scale, 
+            x_scale=parameter.x_scale,
+            y_scale=parameter.y_scale,
             z_scale=parameter.z_scale,
-            #x_range=x_range, TODO: pass range arguments
-            #y_range=y_range,
-            #z_range=z_range,
             train=True)
+
