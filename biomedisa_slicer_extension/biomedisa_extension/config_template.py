d1decda204ee325616d15f0e03bb75e3449920c4
Philipp Loesel <philipp.loesel@anu.edu.au>
2025-01-10 15:47:35 +1100

    pass arguments and use Slicer environment if available

--- biomedisa_slicer_extension/biomedisa_extension/config_template.py
+++ biomedisa_slicer_extension/biomedisa_extension/config_template.py
@@ -2,20 +2,24 @@ import getpass
 username = getpass.getuser()
 
 # default (automatic detection)
-env_path = None
+python_path = None
 lib_path = None
+wsl_path = None
 
 # Windows using WSL
-python_path = "/home/$USER/biomedisa_env/bin/python" # if environment in WSL home directory
-python_path = f"/mnt/c/Users/{username}/biomedisa_env/bin/python" # if environment in Windows User directory
-#env_path = ["wsl","-d","Ubuntu-22.04","-e","bash","-c","export CUDA_HOME=/usr/local/cuda-12.6 && export LD_LIBRARY_PATH=${CUDA_HOME}/lib64 && export PATH=${CUDA_HOME}/bin:${PATH} && " + python_path]
+#python_path = "/home/$USER/biomedisa_env/bin/python" # if the environment is in the WSL home directory
+#python_path = f"/mnt/c/Users/{username}/biomedisa_env/bin/python" # if the environment is in the Windows User directory
+#lib_path = "export CUDA_HOME=/usr/local/cuda-12.6 && export LD_LIBRARY_PATH=${CUDA_HOME}/lib64 && export PATH=${CUDA_HOME}/bin:${PATH}"
+#wsl_path = ["wsl","-d","Ubuntu-22.04","-e","bash","-c"]
 
 # Ubuntu
 # 1st example
-#env_path = f"/home/{username}/biomedisa_env/bin/python"
+#python_path = f"/home/{username}/biomedisa_env/bin/python"
 #lib_path = f"/home/{username}/biomedisa_env/lib/python3.10/site-packages"
+#wsl_path = None
 
 # 2nd example
-#env_path = "/usr/bin/python3"
+#python_path = "/usr/bin/python3"
 #lib_path = f"/home/{username}/.local/lib/python3.10/site-packages"
+#wsl_path = None
 
