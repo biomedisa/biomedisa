278841521f8ee953d3e3dd6c77e13b2357c3ed47
Glenn Myers <grm110@gadi-login-02.gadi.nci.org.au>
2025-02-07 11:36:49 +1100

    Code for 3d rotation.

--- biomedisa/features/DataGenerator.py
+++ biomedisa/features/DataGenerator.py
@@ -32,6 +32,25 @@ import numba
 import random
 import scipy
 
+def get_random_rotation_matrix(batch_size):
+    angle_xy = np.random.uniform(0, 2. * np.pi, batch_size)
+    Householder_1 = np.random.uniform(0, 2. * np.pi, batch_size)
+    Householder_2 = np.random.uniform(0, 1., batch_size)
+    # Matrix for xy rotation
+    RR = np.zeros([batch_size, 3, 3])
+    RR[:, 0, 0] = np.cos(angle_xy[:])
+    RR[:, 0, 1] = np.sin(angle_xy[:])
+    RR[:, 1, 0] = np.cos(angle_xy[:])
+    RR[:, 1, 1] = -np.sin(angle_xy[:])
+    RR[:, 2, 2] = 1.
+    # Householder matrix
+    vv = np.zeros([batch_size, 3, 1])
+    vv[:, 0, 0] = np.cos(Householder_1[:]) * np.sqrt(Householder_2[:])
+    vv[:, 1, 0] = np.sin(Householder_1[:]) * np.sqrt(Householder_2[:])
+    vv[:, 2, 0] = np.sqrt(1. - Householder_2[:])
+    HH = np.eye(3)[np.newaxis, :, :] - 2. * np.matmul(vv, vv.transpose(0, 2, 1))
+    return -np.matmul(HH, RR)
+
 @numba.jit(nopython=True)#parallel=True
 def rotate_img_patch(src,trg,k,l,m,cos_a,sin_a,z_patch,y_patch,x_patch,imageHeight,imageWidth):
     for y in range(l,l+y_patch):
@@ -61,6 +80,57 @@ def rotate_img_patch(src,trg,k,l,m,cos_a,sin_a,z_patch,y_patch,x_patch,imageHeig
                 trg[z-k,y-l,x-m] = val
     return trg
 
+
+@numba.jit(nopython=True)#parallel=True
+def rotate_img_patch_3d(src,trg,k,l,m,rm_xx,rm_xy,rm_xz,rm_yx,rm_yy,rm_yz,rm_zx,rm_zy,rm_zz,z_patch,y_patch,x_patch,imageVertStride,imageDepth,imageHeight,imageWidth):
+    #return rotate_label_patch_3d(src,trg,k,l,m,rm_xx,rm_xy,rm_xz,rm_yx,rm_yy,rm_yz,rm_zx,rm_zy,rm_zz,z_patch,y_patch,x_patch,imageVertStride,imageDepth,imageHeight,imageWidth)
+    for z in range(k,k+z_patch):
+        zCentreRotation = imageVertStride * (z // imageVertStride) + imageVertStride/2
+        zA = z - zCentreRotation
+        for y in range(l,l+y_patch):
+            yA = y - imageHeight/2
+            for x in range(m,m+x_patch):
+                xA = x - imageWidth/2
+                xR = xA * rm_xx + yA * rm_xy + zA * rm_xz
+                yR = xA * rm_yx + yA * rm_yy + zA * rm_yz
+                zR = xA * rm_zx + yA * rm_zy + zA * rm_zz
+                src_x = xR + imageWidth/2
+                src_y = yR + imageHeight/2
+                src_z = zR + zCentreRotation
+                # bilinear interpolation
+                src_x0 = float(int(src_x))
+                src_x1 = src_x0 + 1
+                src_y0 = float(int(src_y))
+                src_y1 = src_y0 + 1
+                src_z0 = float(int(src_z))
+                src_z1 = src_z0 + 1
+                sx = src_x - src_x0
+                sy = src_y - src_y0
+                sz = src_z - src_z0
+                idx_src_x0 = int(min(max(0,src_x0),imageWidth-1))
+                idx_src_x1 = int(min(max(0,src_x1),imageWidth-1))
+                idx_src_y0 = int(min(max(0,src_y0),imageHeight-1))
+                idx_src_y1 = int(min(max(0,src_y1),imageHeight-1))
+                idx_src_z0 = int(min(max(0,src_z0),imageDepth-1))
+                idx_src_z1 = int(min(max(0,src_z1),imageDepth-1))
+                
+                val  = (1-sy) * (1-sx) * (1-sz) * float(src[idx_src_z0,idx_src_y0,idx_src_x0])
+                val += (sy) * (1-sx) * (1-sz) * float(src[idx_src_z0,idx_src_y1,idx_src_x0])
+                val += (1-sy) * (sx) * (1-sz) * float(src[idx_src_z0,idx_src_y0,idx_src_x1])
+                val += (sy) * (sx) * (1-sz) * float(src[idx_src_z0,idx_src_y1,idx_src_x1])
+                val += (1-sy) * (1-sx) * (sz) * float(src[idx_src_z1,idx_src_y0,idx_src_x0])
+                val += (sy) * (1-sx) * (sz) * float(src[idx_src_z1,idx_src_y1,idx_src_x0])
+                val += (1-sy) * (sx) * (sz) * float(src[idx_src_z1,idx_src_y0,idx_src_x1])
+                val += (sy) * (sx) * (sz) * float(src[idx_src_z1,idx_src_y1,idx_src_x1])
+                trg[z-k,y-l,x-m] = val
+    return trg
+
+# This exists so I could test it. It's not called because sometimes numba is funny about nested
+# nopython functions.
+@numba.jit(nopython=True)#parallel=True
+def centre_of_z_rotation(z, imageVertStride):
+    return imageVertStride * (z // imageVertStride) + imageVertStride/2
+
 @numba.jit(nopython=True)#parallel=True
 def rotate_label_patch(src,trg,k,l,m,cos_a,sin_a,z_patch,y_patch,x_patch,imageHeight,imageWidth):
     for y in range(l,l+y_patch):
@@ -78,6 +148,31 @@ def rotate_label_patch(src,trg,k,l,m,cos_a,sin_a,z_patch,y_patch,x_patch,imageHe
             idx_src_y = int(min(max(0,src_y),imageHeight-1))
             for z in range(k,k+z_patch):
                 trg[z-k,y-l,x-m] = src[z,idx_src_y,idx_src_x]
+    return trg
+
+@numba.jit(nopython=True)#parallel=True
+def rotate_label_patch_3d(src,trg,k,l,m,rm_xx,rm_xy,rm_xz,rm_yx,rm_yy,rm_yz,rm_zx,rm_zy,rm_zz,z_patch,y_patch,x_patch,imageVertStride,imageDepth,imageHeight,imageWidth):
+    for z in range(k,k+z_patch):
+        zCentreRotation = imageVertStride * (z // imageVertStride) + imageVertStride/2
+        zA = z - zCentreRotation
+        for y in range(l,l+y_patch):
+            yA = y - imageHeight/2
+            for x in range(m,m+x_patch):
+                xA = x - imageWidth/2
+                xR = xA * rm_xx + yA * rm_xy + zA * rm_xz
+                yR = xA * rm_yx + yA * rm_yy + zA * rm_yz
+                zR = xA * rm_zx + yA * rm_zy + zA * rm_zz
+                src_x = xR + imageWidth/2
+                src_y = yR + imageHeight/2
+                src_z = zR + zCentreRotation
+                # nearest neighbour
+                src_x = round(src_x)
+                src_y = round(src_y)
+                src_z = round(src_z)
+                idx_src_x = int(min(max(0,src_x),imageWidth-1))
+                idx_src_y = int(min(max(0,src_y),imageHeight-1))
+                idx_src_z = int(min(max(0,src_z),imageDepth-1))
+                trg[z-k,y-l,x-m] = src[idx_src_z,idx_src_y,idx_src_x]
     return trg
 
 def random_rotation_3d(image, max_angle=180):
@@ -107,7 +202,7 @@ def random_rotation_3d(image, max_angle=180):
 class DataGenerator(tf.keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, img, label, list_IDs_fg, list_IDs_bg, shuffle, train, classification, batch_size=32, dim=(32,32,32),
-                 dim_img=(32,32,32), n_classes=10, n_channels=1, augment=(False,False,False,False,0), patch_normalization=False, separation=False):
+                 dim_img=(32,32,32), n_classes=10, n_channels=1, augment=(False,False,False,False,0,False), patch_normalization=False, separation=False):
         'Initialization'
         self.dim = dim
         self.dim_img = dim_img
@@ -192,7 +287,7 @@ class DataGenerator(tf.keras.utils.Sequence):
             y = np.empty((self.batch_size, *self.dim, 1), dtype=np.int32)
 
         # get augmentation parameter
-        flip_x, flip_y, flip_z, swapaxes, rotate = self.augment
+        flip_x, flip_y, flip_z, swapaxes, rotate, rotate3d = self.augment
         n_aug = np.sum([flip_z, flip_y, flip_x])
         flips =  np.where([flip_z, flip_y, flip_x])[0]
 
@@ -201,6 +296,8 @@ class DataGenerator(tf.keras.utils.Sequence):
             angle = np.random.uniform(-1,1,self.batch_size) * 3.1416/180*rotate
             cos_angle = np.cos(angle)
             sin_angle = np.sin(angle)
+        if rotate3d:
+            rot_mtx = get_random_rotation_matrix(self.batch_size)
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
@@ -271,6 +368,29 @@ class DataGenerator(tf.keras.utils.Sequence):
                         tmp_y = rotate_label_patch(self.label,tmp_y,k,l,m,cos_a,sin_a,
                             self.dim[0],self.dim[1],self.dim[2],
                             self.dim_img[1],self.dim_img[2])
+                    
+                    # rotate through a random 3d angle, uniformly distributed on a sphere.
+                    if rotate3d:
+                        tmp_X = np.empty((*self.dim, self.n_channels), dtype=np.float32)
+                        tmp_y = np.empty(self.dim, dtype=np.int32)
+                        rm_xx = rot_mtx[i, 0, 0]
+                        rm_xy = rot_mtx[i, 0, 1]
+                        rm_xz = rot_mtx[i, 0, 2]
+                        rm_yx = rot_mtx[i, 1, 0]
+                        rm_yy = rot_mtx[i, 1, 1]
+                        rm_yz = rot_mtx[i, 1, 2]
+                        rm_zx = rot_mtx[i, 2, 0]
+                        rm_zy = rot_mtx[i, 2, 1]
+                        rm_zz = rot_mtx[i, 2, 2]
+                        for c in range(self.n_channels):
+                            tmp_X[:,:,:,c] = rotate_img_patch_3d(self.img[:,:,:,c],tmp_X[:,:,:,c],k,l,m,
+                                rm_xx,rm_xy,rm_xz,rm_yx,rm_yy,rm_yz,rm_zx,rm_zy,rm_zz,
+                                self.dim[0],self.dim[1],self.dim[2],
+                                256, self.dim_img[0],self.dim_img[1],self.dim_img[2])
+                        tmp_y = rotate_label_patch_3d(self.label,tmp_y,k,l,m,
+                            rm_xx,rm_xy,rm_xz,rm_yx,rm_yy,rm_yz,rm_zx,rm_zy,rm_zz,
+                            self.dim[0],self.dim[1],self.dim[2],
+                            256, self.dim_img[0],self.dim_img[1],self.dim_img[2])
 
                     # flip patch along axes
                     v = np.random.randint(n_aug+1)
