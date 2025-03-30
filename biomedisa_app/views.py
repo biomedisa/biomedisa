02ee35c61b64687635767fb4e4c8d0162fefaf79
Philipp Loesel <philipp.loesel@anu.edu.au>
2025-03-30 14:44:47 +1100

    move check for termnation before success check

--- biomedisa_app/views.py
+++ biomedisa_app/views.py
@@ -1043,41 +1043,39 @@ def init_keras_3D(image, label, predict, img_list=None, label_list=None,
                         else:
                             stopped = True
 
-                        # check if successfully completed
-                        if not stopped:
-                            success = subprocess.Popen(['scp', host + ':' + host_base + config_path, BASE_DIR + config_path]).wait()
-
-                        if train and not stopped:
-                            # create objects
-                            for path_to_result, suffix in zip(result_paths, ['.h5', '_acc.png', '_loss.png', '.csv']):
-                                result = subprocess.Popen(['rsync','-avP', host+':'+model_on_host.replace('.h5', suffix), path_to_result]).wait()
-                                if result==0:
-                                    os.chmod(path_to_result, 0o664)
-                                    shortfilename = os.path.basename(path_to_result)
-                                    filename = 'images/' + image.user.username + '/' + shortfilename
-                                    if not Upload.objects.filter(pic=filename).exists():
-                                        Upload.objects.create(pic=filename, user=image.user, project=image.project,
-                                            imageType=(4 if suffix=='.h5' else 6), shortfilename=shortfilename, log=3)
-                            # update processing message
-                            if os.path.exists(result_paths[-1]) and success!=0:
-                                with open(result_paths[-1], "r", encoding="utf-8") as f:
-                                    epochs_trained = 0
-                                    for line in f:
-                                        epochs_trained += 1
-                                image.message = f'Training epoch {epochs_trained} / {label.epochs}'
-                                image.save()
-
                         # check for termination
-                        if not stopped and success!=0:
+                        if not stopped:
                             if sbatch:
                                 monitor = subprocess.Popen(cmd_host + ["squeue", "-j", job_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                             else:
                                 monitor = subprocess.Popen(cmd_host + ["qstat", job_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                            if sbatch or qsub:
-                                stdout, stderr = monitor.communicate()
-                                if monitor.returncode!=0:
-                                    stdout = str(job_id)
+                            stdout, stderr = monitor.communicate()
+                            if monitor.returncode!=0:
+                                stdout = str(job_id)
+
+                            # check if job completed
+                            success = subprocess.Popen(['scp', host + ':' + host_base + config_path, BASE_DIR + config_path]).wait()
                             error = subprocess.Popen(['scp', host + ':' + host_base + error_path, BASE_DIR + error_path]).wait()
+
+                            if train:
+                                # create objects
+                                for path_to_result, suffix in zip(result_paths, ['.h5', '_acc.png', '_loss.png', '.csv']):
+                                    result = subprocess.Popen(['rsync','-avP', host+':'+model_on_host.replace('.h5', suffix), path_to_result]).wait()
+                                    if result==0:
+                                        os.chmod(path_to_result, 0o664)
+                                        shortfilename = os.path.basename(path_to_result)
+                                        filename = 'images/' + image.user.username + '/' + shortfilename
+                                        if not Upload.objects.filter(pic=filename).exists():
+                                            Upload.objects.create(pic=filename, user=image.user, project=image.project,
+                                                imageType=(4 if suffix=='.h5' else 6), shortfilename=shortfilename, log=3)
+                                # update processing message
+                                if os.path.exists(result_paths[-1]) and success!=0:
+                                    with open(result_paths[-1], "r", encoding="utf-8") as f:
+                                        epochs_trained = 0
+                                        for line in f:
+                                            epochs_trained += 1
+                                    image.message = f'Training epoch {min(epochs_trained,label.epochs)} / {label.epochs}'
+                                    image.save()
 
                         # terminate loop
                         if stopped or (job_id not in stdout) or success==0 or error==0:
