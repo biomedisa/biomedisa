022963c005438f7051610d6bb878fba3ce47d84e
Philipp Loesel <philipp.loesel@anu.edu.au>
2026-07-06 18:33:12 +1000

    xy tiling in SAM

--- biomedisa/features/matching/sam_helper.py
+++ biomedisa/features/matching/sam_helper.py
@@ -5,7 +5,7 @@ import time
 import numba
 
 @numba.jit(nopython=True)
-def reduce_blocksize(data, value=1, buff=25):
+def reduce_blocksize(data, value=1, buff=64):
     zsh, ysh, xsh = data.shape
     argmin_z, argmax_z, argmin_y, argmax_y, argmin_x, argmax_x = zsh, 0, ysh, 0, xsh, 0
     for k in range(zsh):
@@ -39,7 +39,7 @@ def get_boundaries(slc):
     grad[grad>0]=1
     return grad
 
-def get_block_bounds(z_dim, block_size=1024, overlap=256):
+def get_block_bounds(z_dim, block_size=1024, overlap=192):
     """
     Iterate over z-blocks with overlap. If the last block would be smaller than half block_size,
     it is merged into the previous block.
@@ -67,6 +67,23 @@ def get_block_bounds(z_dim, block_size=1024, overlap=256):
             block_bounds[-1] = (last_start, z_dim)
 
     return block_bounds
+
+def get_tile_bounds(shape, block_size=1024, overlap=192):
+    """
+    Returns 2D tiles as
+        (y0, y1, x0, x1)
+    """
+    ny, nx = shape
+
+    y_blocks = get_block_bounds(ny, block_size, overlap)
+    x_blocks = get_block_bounds(nx, block_size, overlap)
+
+    tiles = []
+    for y0, y1 in y_blocks:
+        for x0, x1 in x_blocks:
+            tiles.append((y0, y1, x0, x1))
+
+    return tiles
 
 def sam_boundaries(volume=None, volume_path=None, sam_checkpoint=None, boundaries_path=None, mask_path=None):
     TIC = time.time()
@@ -103,25 +120,17 @@ def sam_boundaries(volume=None, volume_path=None, sam_checkpoint=None, boundarie
     print('Data shape:', volume.shape)
     print('Data type:', volume.dtype)
     print('Blocks:', get_block_bounds(zsh))
-    #for dest in range(1, size):
-    #    comm.send([zsh,ysh,xsh], dest=dest)
 
     # initialize boundaries
     if rank==0:
         boundaries = np.zeros(volume.shape, np.uint8)
-
-    #else:
-    #    # receive data size
-    #    zsh, ysh, xsh = comm.recv(source=0)
 
     # Load SAM model
     for m_type in ["vit_h", "vit_l", "vit_b"]:
         if m_type in sam_checkpoint:
             model_type = m_type
     sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-
-    #sam.to("cuda")  # Use "cuda" if a GPU is available, otherwise "cpu"
-    sam.to(device)  # Ensure SAM model loads on the correct GPU
+    sam.to(device)
     print(f"Rank {rank} using GPU {torch.cuda.current_device()}")
 
     # Create a mask generator
@@ -145,44 +154,15 @@ def sam_boundaries(volume=None, volume_path=None, sam_checkpoint=None, boundarie
                 elif axis==2:
                     slc_data = volume[:,:,slc].copy()
 
-                '''# send slices to childs   # WARNING data must be uint16
-                for dest in range(1, size):
-                    if slc+dest < sh:
-                        if axis==0:
-                            comm.Send([volume[slc+dest].copy(), MPI.UNSIGNED_SHORT], dest=dest)
-                        elif axis==1:
-                            comm.Send([volume[:,slc+dest].copy(), MPI.UNSIGNED_SHORT], dest=dest)
-                        elif axis==2:
-                            comm.Send([volume[:,:,slc+dest].copy(), MPI.UNSIGNED_SHORT], dest=dest)
-                else:
-                    # receive slices from parent
-                    if axis==0:
-                        slc_data = np.empty((ysh,xsh), dtype=np.uint16)
-                    elif axis==1:
-                        slc_data = np.empty((zsh,xsh), dtype=np.uint16)
-                    elif axis==2:
-                        slc_data = np.empty((zsh,ysh), dtype=np.uint16)
-                    comm.Recv([slc_data, MPI.UNSIGNED_SHORT], source=0)'''
-
-                #print(f'Rank: {rank}, Axis: {axis}, Slice: {slc+1}/{sh}')
-
                 # determine tiles
-                if axis==0:
-                    blocks = [(0,ysh)]
-                elif axis==1:
-                    blocks = get_block_bounds(zsh)
-                elif axis==2:
-                    blocks = get_block_bounds(zsh)
+                tiles = get_tile_bounds(slc_data.shape)
 
                 # initialize 2d boundaries
                 slc_boundaries = np.zeros(slc_data.shape, np.uint32)
 
                 # iterate over tiles
-                for start, end in blocks:
-                    if axis==0:
-                        data = slc_data
-                    else:
-                        data = slc_data[start:end].copy()
+                for y0, y1, x0, x1 in tiles:
+                    data = slc_data[y0:y1, x0:x1].copy()
 
                     # scale data
                     data = data.astype(np.float32)
@@ -201,17 +181,17 @@ def sam_boundaries(volume=None, volume_path=None, sam_checkpoint=None, boundarie
                     masks = mask_generator.generate(image)
 
                     # determine buffer size
-                    sbuff = 64 if start>0 else 0
-                    ebuff = 64 if end<zsh else 0
-                    start = start+sbuff
-                    end = end-ebuff
+                    buff = 64
+                    top    = buff if y0 > 0 else 0
+                    bottom = buff if y1 < slc_data.shape[0] else 0
+                    left   = buff if x0 > 0 else 0
+                    right  = buff if x1 < slc_data.shape[1] else 0
 
                     # iterate over masks
                     for l, mask in enumerate(masks):
-                        if axis==0:
-                            slc_boundaries += get_boundaries(mask['segmentation'].astype(np.uint8))
-                        else:
-                            slc_boundaries[start:end] += get_boundaries(mask['segmentation'].astype(np.uint8))[sbuff:sbuff+end-start]
+                        tile_boundaries = get_boundaries(mask["segmentation"].astype(np.uint8))
+                        crop = tile_boundaries[top:top+(y1-bottom)-(y0+top), left:left+(x1-right)-(x0+left)]
+                        slc_boundaries[y0+top:y1-bottom, x0+left:x1-right] += crop
 
                 # binarize boundary
                 slc_boundaries[slc_boundaries>0]=1
@@ -255,104 +235,4 @@ def sam_boundaries(volume=None, volume_path=None, sam_checkpoint=None, boundarie
         if boundaries_path:
             save_data(boundaries_path, output)
         return output
-
-def sam_slices(volume=None, volume_path=None, sam_checkpoint=None, mask_path=None):
-    TIC = time.time()
-    from mpi4py import MPI
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    #os.environ["CUDA_VISIBLE_DEVICES"] = str(rank)
-    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-
-    import torch
-    device = torch.device(f"cuda:{rank}")
-    torch.cuda.set_device(device)
-    from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
-
-    # load mask data
-    if mask_path:
-        mask = imread(mask_path)
-        argmin_z, argmax_z, argmin_y, argmax_y, argmin_x, argmax_x = reduce_blocksize(mask)
-        del mask
-
-    # load data
-    if volume is None:
-        volume = imread(volume_path)   # TODO: WARNING data must be uint16 for Multi-GPU
-
-    # crop volume to mask
-    if mask_path:
-        z_shape, y_shape, x_shape = volume.shape
-        volume = volume[argmin_z:argmax_z, argmin_y:argmax_y, argmin_x:argmax_x].copy()
-
-    # data size
-    zsh, ysh, xsh = volume.shape
-    print('Data shape:', volume.shape)
-    print('Data type:', volume.dtype)
-    print('Blocks:', get_block_bounds(zsh))
-    #for dest in range(1, size):
-    #    comm.send([zsh,ysh,xsh], dest=dest)
-
-    # initialize boundaries
-    if rank==0:
-        result = np.zeros(volume.shape, np.uint16)
-
-    #else:
-    #    # receive data size
-    #    zsh, ysh, xsh = comm.recv(source=0)
-
-    # Load SAM model
-    for m_type in ["vit_h", "vit_l", "vit_b"]:
-        if m_type in sam_checkpoint:
-            model_type = m_type
-    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-    #sam.to("cuda")  # Use "cuda" if a GPU is available, otherwise "cpu"
-    sam.to(device)  # Ensure SAM model loads on the correct GPU
-    print(f"Rank {rank} using GPU {torch.cuda.current_device()}")
-
-    # Create a mask generator
-    mask_generator = SamAutomaticMaskGenerator(sam, points_per_side=64)
-
-    # iterate over views
-    for axis, sh in enumerate([zsh]):
-        # iterate over slices
-        for slc in range(0,sh,20):
-            if slc % size == rank:
-
-                if rank==0:
-                    print(f'Axis: {axis}, Slice: {slc+1}/{sh}       ', end='\r')
-
-                # extract slice
-                if axis==0:
-                    data = volume[slc].copy()
-                elif axis==1:
-                    data = volume[:,slc].copy()
-                elif axis==2:
-                    data = volume[:,:,slc].copy()
-
-                print(f'Rank: {rank}, Axis: {axis}, Slice: {slc+1}/{sh}')
-
-                # scale data
-                data = data.astype(np.float32)
-                data -= np.amin(data)
-                if np.amax(data)>0:
-                    data /= np.amax(data)
-                data *= 255.0
-                data = data.astype(np.uint8)
-
-                # convert data to pseudo RGB
-                image = np.empty(data.shape + (3,), np.uint8)
-                for k in range(3):
-                    image[:,:,k] = data
-
-                # run segmentation
-                masks = mask_generator.generate(image)
-
-                # iterate over masks
-                for l, mask in enumerate(masks):
-                    if axis==0:
-                        result[slc][mask['segmentation']>0]=l
-
-    return result
 
